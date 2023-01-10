c5ebb931b966162c7a0e422c0f50b7b0a0dcb55b
Christopher <christopher@bioinf.uni-leipzig.de>
2023-01-10 14:52:02 +0100

    add code for manual installation

---
+++ install_svhip.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+THIS_DIR=$(basename $(pwd))
+cd ..
+mkdir -p $CONDA_PREFIX/share
+cp -r $THIS_DIR $CONDA_PREFIX/share/Svhip
+mkdir -p $CONDA_PREFIX/bin/
+echo "#!/bin/bash" >> $CONDA_PREFIX/bin/svhip
+echo "SHARE_DIR=$CONDA_PREFIX/share/Svhip" >> $CONDA_PREFIX/bin/svhip
+head -1 $CONDA_PREFIX/share/Svhip/run_svhip.sh >> $CONDA_PREFIX/bin/svhip
+chmod +x $CONDA_PREFIX/bin/svhip
